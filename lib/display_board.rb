9a9db9b6d5f29b3e88dd86b1d6e1f39d70c73ce8
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-11 18:43:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,5 @@
 # out the current state.
 board = ["   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "]
 def display_board(board)
-  puts board
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
