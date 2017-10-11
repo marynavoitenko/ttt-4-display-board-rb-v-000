e3af650595a0cd3c4ef7e2d403f1dac9cf07a265
Maryna Voitenko <maryna.voitenko@gmail.com>
2017-10-11 18:38:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = ["   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "]
-def display_board
-  
+def display_board(board)
+  puts board
 end
